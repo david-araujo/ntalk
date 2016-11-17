e41af00d704c69f80ddf08edd7444961c7c6c4b4
David Araujo <davsra@br.ibm.com>
2016-11-17 18:28:19 -0200

    Removed old Express methods

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,25 @@
 module.exports = function(app) {
-  var HomeController = {
-    index: function(req, res) {
-      res.render('home/index');
-} };
-  return HomeController;
+    var User = app.models.user;
+    var HomeController = {
+        index: function(req, res) {
+            res.render('home/index');
+        },
+        login: function(req, res) {
+            var email = req.body.user.email,
+                name = req.body.user.name;
+            if (email && name) {
+                var user = req.body.user;
+                user['contacts'] = [];
+                req.session.user = user;
+                res.redirect('/contacts');
+            } else {
+                res.redirect('/');
+            }
+        },
+        logout: function(req, res) {
+            req.session.destroy();
+            res.redirect('/');
+        }
+    };
+    return HomeController;
 };
